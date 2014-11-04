ec5dedd8692387bc5136197c631072db9ee3d67b
Pkthunder <jared_perreault@yahoo.com>
2014-11-04 11:36:10 -0500

    In-Class Assignment 1

---
+++ InClassAssignment1/application.js
@@ -0,0 +1,78 @@
+// This script implements simple routing by loading partial HTML files 
+// named corresponding to fragment identifiers.
+
+$(document).ready( function() {
+
+
+  // Sets the "active" class on the active navigation link.
+  function setActiveLink(fragmentId) {
+    $("#nav-table li.nav-btn").each(function (i, linkElement) {
+      var link = $(this).find("a");
+      var pageName = link.attr("href").substr(1);
+
+      if (pageName === fragmentId) {
+        link.addClass("active");
+      } 
+      else {
+        link.removeClass("active");
+      }
+    });
+  }
+
+  // Binds page specfic events to newly loaded pages
+  function onSuccess(fragmentId) {
+  	if (fragmentId === "home") {
+
+  	}
+  	else if (fragmentId === "big-fudge") {
+
+  	}
+  	else if (fragmentId === "houses") {
+  		//Fill in the house's specific name
+  		var strName = $("img.img-format:first").attr("data-name");
+  		$("#house-name").text(strName);
+  		//Bind click events to tiny images
+  		$("img.img-format").bind("click", function() {
+  			var strImg = $(this).attr("src");
+  			$("#main-img").attr("src", strImg);
+  			var strNewName = $(this).attr("data-name");
+  			$("#house-name").text(strNewName);
+  		});
+  	}
+  	else {
+  		alert("Error!");
+  	}
+  }
+
+  // Updates dynamic content based on the fragment identifier.
+  function navigate() {
+
+    // Isolate the fragment identifier using substr.
+    // This gets rid of the "#" character.
+    var fragmentId = location.hash.substr(1);
+
+    // Loads the partial HTML page and adds it into "#content"
+    // @function() "on success" binds/runs page events to the specific page
+    $("#content").load(fragmentId + ".html", function() {
+    	onSuccess(fragmentId);
+    });
+
+    // Toggle the "active" class on the link currently navigated to.
+    setActiveLink(fragmentId);
+
+  }
+
+  // If no fragment identifier is provided,
+  if(!location.hash) {
+
+    // default to #home.
+    location.hash = "#home";
+  }
+
+  // Navigate once to the initial fragment identifier.
+  navigate();
+
+  // Navigate whenever the fragment identifier value changes.
+  $(window).bind('hashchange', navigate);
+
+}); //end of $(document).ready
--- InClassQuiz1/application.js
+++ InClassQuiz1/application.js
@@ -3,10 +3,8 @@ $(document).ready( function() {
 	$("#m-form").submit( function() {
 		$("#p-wrapper p").addClass("hide");
 
-		var inStr = $("#m-input").val();
-
 		setTimeout(  function() {
-			(inStr === "Kdubzz fo life") ? $("#pass").removeClass("hide") : $("#fail").removeClass("hide");
+			($("#m-input").val() === "Kdubzz fo life") ? $("#pass").removeClass("hide") : $("#fail").removeClass("hide");
 		}, 100);
 
 	});
